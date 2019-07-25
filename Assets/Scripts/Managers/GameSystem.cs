7947489218ea8a94ac0f5f501394dfe8b97e26f5
Sebastien Gaumin <sgaumin@gmail.com>
2019-07-25 22:20:05 +0200

    Implemented Quit game button.

--- Assets/Scripts/Managers/GameSystem.cs
+++ Assets/Scripts/Managers/GameSystem.cs
@@ -14,4 +14,15 @@ public class GameSystem : MonoBehaviour
 		DOTween.defaultAutoPlay = AutoPlay.None;
 		DOTween.defaultAutoKill = false;
 	}
+
+	protected void Update()
+	{
+#if UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX
+
+		if (Input.GetButtonDown("Quit"))
+		{
+			LevelLoader.Instance.QuitGame();
+		}
+#endif
+	}
 }
