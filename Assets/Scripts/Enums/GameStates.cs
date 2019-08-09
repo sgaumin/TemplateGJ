dbcca0bfa49d426fb41c9cd7c79d3f6fecbad869
Sebastien Gaumin <sgaumin@gmail.com>
2019-08-09 19:53:54 +0200

    [Cleaning] Useful scripts placed into Tools sub-module.

--- Assets/Scripts/Enums/GameStates.cs
+++
@@ -1,6 +0,0 @@
-﻿public enum GameStates
-{
-	Play,
-	GameOver,
-	Pause
-}
---
+++ Assets/Scripts/Managers/Game.cs
@@ -0,0 +1,12 @@
+﻿public class Game : GameSystem
+{
+	public static Game Instance { get; private set; }
+
+	public GameStates GameState { get; private set; } = GameStates.Play;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		Instance = this;
+	}
+}
