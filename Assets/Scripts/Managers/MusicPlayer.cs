37bf8fe178cf1a3aa223e168eb8ec99d98ea7822
Sebastien Gaumin <sgaumin@gmail.com>
2019-12-16 13:29:50 +0100

    [Audio] Music Player prefab created.

---
+++ Assets/Scripts/Managers/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+	public static MusicPlayer Instance { get; private set; }
+
+	protected void Awake()
+	{
+		if (Instance == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			Instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+}
