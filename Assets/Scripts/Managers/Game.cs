3743ac64f2506f665398debb77c245d703e14387
Sebastien Gaumin <sgaumin@gmail.com>
2019-12-16 13:20:38 +0100

    [Refacto] Delegate declared.

--- Assets/Scripts/Managers/Game.cs
+++ Assets/Scripts/Managers/Game.cs
@@ -14,12 +14,46 @@ public class Game : GameSystem
 {
 	public static Game Instance { get; private set; }
 
-	public GameStates GameState { get; private set; } = GameStates.Play;
+	public delegate void GameEventHandler();
+	public event GameEventHandler OnStart;
+	public event GameEventHandler OnGameOver;
+	public event GameEventHandler OnPause;
+
+	private GameStates gameState;
+
+	public GameStates GameState
+	{
+		get => gameState;
+		set
+		{
+			gameState = value;
+
+			switch (value)
+			{
+				case GameStates.Play:
+					OnStart?.Invoke();
+					break;
+
+				case GameStates.GameOver:
+					OnGameOver?.Invoke();
+					break;
+
+				case GameStates.Pause:
+					OnPause?.Invoke();
+					break;
+			}
+		}
+	}
 
 	protected override void Awake()
 	{
 		base.Awake();
 		Instance = this;
+	}
+
+	protected void Start()
+	{
+		GameState = GameStates.Play;
 	}
 
 	protected override void Update()
