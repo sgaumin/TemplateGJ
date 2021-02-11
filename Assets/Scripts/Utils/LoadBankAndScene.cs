44becaa1be92f266813ddc8d856a07f2b85e9129
Sebastien Gm <sgaumin@gmail.com>
2021-02-11 18:07:51 +0900

    [Build] Loading scene addition

---
+++ Assets/Scripts/Utils/LoadBankAndScene.cs
@@ -0,0 +1,20 @@
+﻿using Tools;
+using UnityEngine;
+
+public class LoadBankAndScene : MonoBehaviour
+{
+	[SerializeField, FMODUnity.BankRef] private string bankName;
+
+	private void Awake()
+	{
+		FMODUnity.RuntimeManager.LoadBank(bankName, true);
+	}
+
+	void Update()
+	{
+		if (FMODUnity.RuntimeManager.HasBankLoaded(bankName))
+		{
+			LevelLoader.LoadNextLevel();
+		}
+	}
+}
