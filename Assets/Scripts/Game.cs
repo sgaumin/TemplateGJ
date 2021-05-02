4b0d99f1512c73ad6d3ffd34652e824e31caeb20
Sebastien Gm <sgaumin@gmail.com>
2021-05-02 09:37:27 +0900

    [Audio] Mixing methods set

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,20 +1,31 @@
 using System;
 using System.Collections;
 using Tools;
+using Tools.Utils;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Game : GameSystem
 {
+	public const string GAME_MUSIC_VOLUME = "musicVolume";
+
 	public delegate void GameEventHandler();
 	public event GameEventHandler OnStart;
 	public event GameEventHandler OnGameOver;
 	public event GameEventHandler OnPause;
+
+	[Header("Audio")]
+	[SerializeField] private AudioMixer mixer;
+	[Space]
+	[SerializeField] private AudioExpress gameMusic;
+	[SerializeField, FloatRangeSlider(-80f, 10f)] private FloatRange gameMusicVolumeLimits = new FloatRange(-60f, 0f);
 
 	[Header("References")]
 	[SerializeField] private FadScreen fader;
 
 	private GameStates gameState;
 	private Coroutine loadingLevel;
+	private float gameMusicVolume;
 
 	public GameStates GameState
 	{
@@ -50,11 +61,23 @@ public class Game : GameSystem
 	{
 		GameState = GameStates.Play;
 		fader.FadIn();
+
+		mixer.GetFloat(GAME_MUSIC_VOLUME, out gameMusicVolume);
+
+		gameMusic.Play();
 	}
 
 	protected override void Update()
 	{
 		base.Update();
+	}
+	#endregion
+
+	#region Audio
+	public void UpdateGameMusicVolume(float percentage)
+	{
+		gameMusicVolume = Mathf.Lerp(gameMusicVolumeLimits.Min, gameMusicVolumeLimits.Max, percentage);
+		mixer.SetFloat(GAME_MUSIC_VOLUME, gameMusicVolume);
 	}
 	#endregion
 
