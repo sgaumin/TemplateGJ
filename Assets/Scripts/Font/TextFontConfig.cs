1c79ecd9de08c2ec831818190a6b9ca8e55c6f1d
Sebastien <sgaumin@gmail.com>
2021-07-03 10:36:36 +0900

    [Feature] Font setter scripts

---
+++ Assets/Scripts/Font/TextFontConfig.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TextFontConfig : MonoBehaviour
+{
+	public static TextFontConfig Instance { get; private set; }
+
+	protected void Awake() => Instance = this;
+
+	[SerializeField] private TMP_FontAsset tmpFont;
+	[SerializeField] private Font plainFont;
+
+	public TMP_FontAsset TMPFont => tmpFont;
+	public Font PlainFont => plainFont;
+}
---
+++ Assets/Scripts/Font/TextFontSetter.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFontSetter : MonoBehaviour
+{
+	private TextMeshProUGUI tmpText;
+	private Text plainText;
+
+	protected void Start()
+	{
+		tmpText = GetComponent<TextMeshProUGUI>();
+		plainText = GetComponent<Text>();
+		SetFont();
+	}
+
+	public void SetFont()
+	{
+		if (tmpText != null)
+			tmpText.font = TextFontConfig.Instance.TMPFont;
+		if (plainText != null)
+			plainText.font = TextFontConfig.Instance.PlainFont;
+	}
+}
