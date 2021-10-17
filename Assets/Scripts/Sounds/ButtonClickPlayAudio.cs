0546ec223dcfb56282484a200e7002c621cf0ea8
Sebastien <sgaumin@gmail.com>
2021-10-17 12:45:04 +0900

    [Naming] ButtonClickPlayAudio update

---
+++ Assets/Scripts/Sounds/ButtonClickPlayAudio.cs
@@ -0,0 +1,28 @@
+﻿using Tools.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClickPlayAudio : MonoBehaviour
+{
+	[SerializeField] private bool autoAssignButton;
+	[SerializeField] private AudioExpress sound;
+
+	private Button button;
+
+	protected void Awake()
+	{
+		if (autoAssignButton)
+		{
+			button = GetComponent<Button>();
+			if (button != null)
+			{
+				button.onClick.AddListener(Play);
+			}
+		}
+	}
+
+	public void Play()
+	{
+		sound.Play();
+	}
+}
