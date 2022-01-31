f3bf3c3e56c69fffd44873b475d3a00519215e04
Sebastien <sgaumin@gmail.com>
2022-01-31 11:02:43 +0900

    Packages & Scripts updates

---
+++ Assets/Scripts/Utils/OpenURL.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Tools.Utils
+{
+	public static class OpenURL
+	{
+		public static void Open(string link)
+		{
+			if (!string.IsNullOrEmpty(link))
+			{
+				Application.OpenURL(link);
+			}
+		}
+
+		public static void OpenTwitterProfile()
+		{
+			Application.OpenURL(Constants.TWITTER_PROFILE);
+		}
+
+		public static void OpenItchProfile()
+		{
+			Application.OpenURL(Constants.ITCH_PROFILE);
+		}
+
+		public static void OpenGitHubProfile()
+		{
+			Application.OpenURL(Constants.GITHUB_PROFILE);
+		}
+	}
+}
