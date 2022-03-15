5c42d1fa52429628e97c75fed963d14b6956b677
Sébastien Gaumin <sgaumin@gmail.com>
2022-03-15 17:29:07 +0900

    Coroutine extensions methods

--- Assets/Scripts/Managers/LevelController.cs
+++ Assets/Scripts/Managers/LevelController.cs
@@ -1,4 +1,14 @@
+using Cinemachine;
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering.PostProcessing;
 using Utils;
+using Utils.Dependency;
+using static Facade;
 
 public class LevelController : SceneBase, ISingleton
 {
