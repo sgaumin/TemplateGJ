314aaebcbecf4d262567ed61c3c1ab5449ab9feb
Sébastien Gaumin <sgaumin@gmail.com>
2022-04-10 22:16:15 +0900

    Uppercase methods setup

--- Assets/Scripts/Utils/Extensions/Vector2Extensions.cs
+++ Assets/Scripts/Utils/Extensions/Vector2Extensions.cs
@@ -4,17 +4,17 @@ namespace Utils
 {
 	public static class Vector2Extensions
 	{
-		public static Vector2 withX(this Vector2 vector, float x) => new Vector2(x, vector.y);
+		public static Vector2 WithX(this Vector2 vector, float x) => new Vector2(x, vector.y);
 
-		public static Vector2 withY(this Vector2 vector, float y) => new Vector2(vector.x, y);
+		public static Vector2 WithY(this Vector2 vector, float y) => new Vector2(vector.x, y);
 
-		public static Vector2 plusX(this Vector2 vector, float plusX) => new Vector2(vector.x + plusX, vector.y);
+		public static Vector2 PlusX(this Vector2 vector, float plusX) => new Vector2(vector.x + plusX, vector.y);
 
-		public static Vector2 plusY(this Vector2 vector, float plusY) => new Vector2(vector.x, vector.y + plusY);
+		public static Vector2 PlusY(this Vector2 vector, float plusY) => new Vector2(vector.x, vector.y + plusY);
 
-		public static Vector2 timesX(this Vector2 vector, float timesX) => new Vector2(vector.x * timesX, vector.y);
+		public static Vector2 TimesX(this Vector2 vector, float timesX) => new Vector2(vector.x * timesX, vector.y);
 
-		public static Vector2 timesY(this Vector2 vector, float timesY) => new Vector2(vector.x, vector.y * timesY);
+		public static Vector2 TimesY(this Vector2 vector, float timesY) => new Vector2(vector.x, vector.y * timesY);
 
 		public static Vector2 Rotate(this Vector2 v, float degrees)
 		{
--- Assets/Scripts/Utils/Extensions/Vector3Extensions.cs
+++ Assets/Scripts/Utils/Extensions/Vector3Extensions.cs
@@ -4,22 +4,22 @@ namespace Utils
 {
 	public static class Vector3Extensions
 	{
-		public static Vector3 withX(this Vector3 vector, float x) => new Vector3(x, vector.y, vector.z);
+		public static Vector3 WithX(this Vector3 vector, float x) => new Vector3(x, vector.y, vector.z);
 
-		public static Vector3 withY(this Vector3 vector, float y) => new Vector3(vector.x, y, vector.z);
+		public static Vector3 WithY(this Vector3 vector, float y) => new Vector3(vector.x, y, vector.z);
 
-		public static Vector3 withZ(this Vector3 vector, float z) => new Vector3(vector.x, vector.y, z);
+		public static Vector3 WithZ(this Vector3 vector, float z) => new Vector3(vector.x, vector.y, z);
 
-		public static Vector3 plusX(this Vector3 vector, float plusX) => new Vector3(vector.x + plusX, vector.y, vector.z);
+		public static Vector3 PlusX(this Vector3 vector, float plusX) => new Vector3(vector.x + plusX, vector.y, vector.z);
 
-		public static Vector3 plusY(this Vector3 vector, float plusY) => new Vector3(vector.x, vector.y + plusY, vector.z);
+		public static Vector3 PlusY(this Vector3 vector, float plusY) => new Vector3(vector.x, vector.y + plusY, vector.z);
 
-		public static Vector3 plusZ(this Vector3 vector, float plusZ) => new Vector3(vector.x, vector.y, vector.z + plusZ);
+		public static Vector3 PlusZ(this Vector3 vector, float plusZ) => new Vector3(vector.x, vector.y, vector.z + plusZ);
 
-		public static Vector3 timesX(this Vector3 vector, float timesX) => new Vector3(vector.x * timesX, vector.y, vector.z);
+		public static Vector3 TimesX(this Vector3 vector, float timesX) => new Vector3(vector.x * timesX, vector.y, vector.z);
 
-		public static Vector3 timesY(this Vector3 vector, float timesY) => new Vector3(vector.x, vector.y * timesY, vector.z);
+		public static Vector3 TimesY(this Vector3 vector, float timesY) => new Vector3(vector.x, vector.y * timesY, vector.z);
 
-		public static Vector3 timesZ(this Vector3 vector, float timesZ) => new Vector3(vector.x, vector.y, vector.z * timesZ);
+		public static Vector3 TimesZ(this Vector3 vector, float timesZ) => new Vector3(vector.x, vector.y, vector.z * timesZ);
 	}
 }
