61d4118a5d508145dd22e6ee8f3c07d0a1e85478
Sébastien Gaumin <sgaumin@gmail.com>
2022-04-10 22:49:15 +0900

    Links menu

--- Assets/Scripts/Data/Constants.cs
+++ Assets/Scripts/Data/Constants.cs
@@ -5,4 +5,5 @@
 	public static string TWITTER_PROFILE = "https://twitter.com/mahjoub_gamedev";
 	public static string ITCH_PROFILE = "https://mahjoub.itch.io/";
 	public static string GITHUB_PROFILE = "https://github.com/sgaumin";
+	public static string GITHUB_PROJECT = "https://github.com/sgaumin";
 }
