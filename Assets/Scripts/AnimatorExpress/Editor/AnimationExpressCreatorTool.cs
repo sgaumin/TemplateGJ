e21a38824c262eb8d81cb8bc5b97980641d9a10b
Sébastien Gaumin <sgaumin@gmail.com>
2022-10-09 12:19:17 +0900

    Option for updating sprites names of a spriteSheets

--- Assets/Scripts/AnimatorExpress/Editor/AnimationExpressCreatorTool.cs
+++ Assets/Scripts/AnimatorExpress/Editor/AnimationExpressCreatorTool.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
@@ -19,15 +20,6 @@ namespace AnimExpress
 			string path = AssetDatabase.GetAssetPath(textureImporter);
 			string folderPath = Path.GetDirectoryName(path);
 			string fileName = Path.GetFileName(path).Replace(".png", "");
-			Sprite[] sprites = AssetDatabase.LoadAllAssetsAtPath(path).OfType<Sprite>().ToArray();
-
-			List<Frame> frames = new List<Frame>();
-			for (int i = 0; i < sprites.Length; i++)
-			{
-				Frame frame = new Frame(sprites[i]);
-				frames.Add(frame);
-			}
-			AnimationExpress animClip = new AnimationExpress(frames);
 
 			string newFodlerPath = Path.Combine(folderPath, SUBFODLER);
 			if (!Directory.Exists(newFodlerPath))
@@ -37,12 +29,44 @@ namespace AnimExpress
 
 			string newAssetName = Path.Combine(newFodlerPath, $"{fileName}.asset");
 			var existingAsset = AssetDatabase.LoadAssetAtPath<AnimationExpress>(newAssetName);
-			if (existingAsset is null)
+			if (existingAsset is not null) return;
+
+			UpdateSpriteName(command);
+
+			Sprite[] sprites = AssetDatabase.LoadAllAssetsAtPath(path).OfType<Sprite>().ToArray();
+			sprites = sprites.OrderBy(x => x.name).ToArray();
+
+			List<Frame> frames = new List<Frame>();
+			for (int i = 0; i < sprites.Length; i++)
 			{
-				AssetDatabase.CreateAsset(animClip, newAssetName);
-				AssetDatabase.SaveAssets();
-				AssetDatabase.Refresh();
+				Frame frame = new Frame(sprites[i]);
+				frames.Add(frame);
 			}
+			AnimationExpress animClip = new AnimationExpress(frames);
+
+			AssetDatabase.CreateAsset(animClip, newAssetName);
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+		}
+
+		[MenuItem("CONTEXT/TextureImporter/Update Sprite Name", priority = 999)]
+		private static void UpdateSpriteName(MenuCommand command)
+		{
+			TextureImporter textureImporter = (TextureImporter)command.context;
+			string path = AssetDatabase.GetAssetPath(textureImporter);
+			string fileName = Path.GetFileName(path).Replace(".png", "");
+
+			SpriteMetaData[] spritesheet = textureImporter.spritesheet;
+			for (int i = 0; i < spritesheet.Length; i++)
+			{
+				spritesheet[i].name = $"{fileName}_{i}";
+			}
+			textureImporter.spritesheet = spritesheet;
+
+			EditorUtility.SetDirty(textureImporter);
+			AssetDatabase.Refresh();
+			AssetDatabase.SaveAssets();
+			textureImporter.SaveAndReimport();
 		}
 	}
 }
