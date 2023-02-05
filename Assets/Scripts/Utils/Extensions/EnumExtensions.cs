ad2259a38e8bfe05cd4c5489d43139321f60487e
Sébastien Gaumin <sgaumin@gmail.com>
2023-02-05 23:20:30 +0900

    Adding new Enum extension methods

--- Assets/Scripts/Utils/Extensions/EnumExtensions.cs
+++ Assets/Scripts/Utils/Extensions/EnumExtensions.cs
@@ -16,5 +16,29 @@ namespace Utils
 		{
 			return (T)Enum.Parse(typeof(T), value, true);
 		}
+
+		/// <summary>
+		/// Get next entry of this enum.
+		/// </summary>
+		public static T Next<T>(this T src) where T : struct
+		{
+			if (!typeof(T).IsEnum) throw new ArgumentException(String.Format("Argument {0} is not an Enum", typeof(T).FullName));
+
+			T[] Arr = (T[])Enum.GetValues(src.GetType());
+			int j = Array.IndexOf<T>(Arr, src) + 1;
+			return (Arr.Length == j) ? Arr[0] : Arr[j];
+		}
+
+		/// <summary>
+		/// Get previous entry of this enum.
+		/// </summary>
+		public static T Previous<T>(this T src) where T : struct
+		{
+			if (!typeof(T).IsEnum) throw new ArgumentException(String.Format("Argument {0} is not an Enum", typeof(T).FullName));
+
+			T[] Arr = (T[])Enum.GetValues(src.GetType());
+			int j = Array.IndexOf<T>(Arr, src) - 1;
+			return (j < 0) ? Arr[Arr.Length - 1] : Arr[j];
+		}
 	}
 }
