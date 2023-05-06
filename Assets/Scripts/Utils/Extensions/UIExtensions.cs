c7d9ae36be4b1efdebf9d67621a70f46ab919517
Sébastien Gaumin <sgaumin@gmail.com>
2023-05-06 11:47:56 +0900

    Refactor UI extensions naming methods

--- Assets/Scripts/Utils/Extensions/UIExtensions.cs
+++ Assets/Scripts/Utils/Extensions/UIExtensions.cs
@@ -7,48 +7,48 @@ namespace Utils
 	public static class UIExtensions
 	{
 		/// <summary>
-		/// Assign a new color from the given TextMeshPro and a new value for its alpha.
+		/// Sets a new alpha value on a given TextMeshPro.
 		/// </summary>
-		/// <param name="t">TextMeshPro</param>
+		/// <param name="t">TextMeshPro reference</param>
 		/// <param name="a">The new alpha value</param>
 		/// <returns>A new color {t.rgb, a}</returns>
-		public static TextMeshPro WithAlpha(this TextMeshPro t, float a)
+		public static TextMeshPro SetAlpha(this TextMeshPro t, float a)
 		{
 			t.color = t.color.WithAlpha(a);
 			return t;
 		}
 
 		/// <summary>
-		/// Assign a new color from the given TextMeshProUGUI and a new value for its alpha.
+		/// Sets a new alpha value on a given TextMeshProUGUI.
 		/// </summary>
-		/// <param name="t">TextMeshProUGUI</param>
+		/// <param name="t">TextMeshProUGUI reference</param>
 		/// <param name="a">The new alpha value</param>
 		/// <returns>A new color {t.rgb, a}</returns>
-		public static TextMeshProUGUI WithAlpha(this TextMeshProUGUI t, float a)
+		public static TextMeshProUGUI SetAlpha(this TextMeshProUGUI t, float a)
 		{
 			t.color = t.color.WithAlpha(a);
 			return t;
 		}
 
 		/// <summary>
-		/// Assign a new color from the given SpriteRenderer and a new value for its alpha.
+		/// Sets a new alpha value on a given SpriteRenderer.
 		/// </summary>
-		/// <param name="s">SpriteRenderer</param>
+		/// <param name="s">SpriteRenderer reference</param>
 		/// <param name="a">The new alpha value</param>
 		/// <returns>A new color {s.rgb, a}</returns>
-		public static SpriteRenderer WithAlpha(this SpriteRenderer s, float a)
+		public static SpriteRenderer SetAlpha(this SpriteRenderer s, float a)
 		{
 			s.color = s.color.WithAlpha(a);
 			return s;
 		}
 
 		/// <summary>
-		/// Assign a new color from the given Image and a new value for its alpha.
+		/// Sets a new alpha value on a given Image.
 		/// </summary>
-		/// <param name="i">Image</param>
+		/// <param name="i">Image reference</param>
 		/// <param name="a">The new alpha value</param>
 		/// <returns>A new color {i.rgb, a}</returns>
-		public static Image WithAlpha(this Image i, float a)
+		public static Image SetAlpha(this Image i, float a)
 		{
 			i.color = i.color.WithAlpha(a);
 			return i;
